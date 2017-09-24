2d82c56198ae4aa83d1e3d9b47ae0fb648db9a4d
Hugo Musso Gualandi <hgualandi@inf.puc-rio.br>
2017-09-24 01:52:14 -0300

    Get rid of ".ast" files in parser tests
    Now the tests for the parser only look at the part of the AST related to
    the feature they are testing and don't care if the structure of an
    unrelated part changes.
    
    This should hopefuly reduce the number of tests that break when we
    update the AST, as well as the clutter in the patch diffs.

--- spec/parser_spec.lua
+++ spec/parser_spec.lua
@@ -1,47 +1,184 @@
 local parser = require 'titan-compiler.parser'
 local util = require 'titan-compiler.util'
 
-local function assert_parse(sourcefilename)
-    local source   = assert(util.get_file_contents(sourcefilename))
-    local expected = assert(util.get_file_contents(sourcefilename..".ast"))
-    local ast, errors = parser.parse(source)
-    local obtained
-    if ast then
-        obtained = parser.pretty_print_ast(ast).."\n"
+-- Return a version of t2 that only contains fields present in t1 (recursively)
+-- Example:
+--   t1  = { b = { c = 10 } e = 40 }
+--   t2  = { a = 1, b = { c = 20, d = 30} }
+--   out = { b = { c = 20 } }
+local function restrict(t1, t2)
+    if type(t1) == 'table' and type(t2) == 'table' then
+        local out = {}
+        for k,_ in pairs(t1) do
+            out[k] = restrict(t1[k], t2[k])
+        end
+        return out
     else
-        error("not yet implemented")
+        return t2
     end
-    assert.are.same(expected, obtained)
+end
+
+local function parse_file(filename)
+    local input = assert(util.get_file_contents(filename))
+    return parser.parse(input)
+end
+
+-- To avoid having these tests break all the time when we make insignificant
+-- changes to the AST, we only verify a subset of the AST.
+local function assert_ast(program, expected)
+    local received = restrict(expected, program)
+    assert.are.same(expected, received)
 end
 
 describe("Titan parser", function()
     
     it("can parse toplevel var declarations", function()
-        assert_parse("./testfiles/toplevel_var.titan")
+        local program, err = parse_file("./testfiles/toplevel_var.titan")
+        assert.truthy(program)
+        assert_ast(program, {
+            { _tag = "TopLevel_Var",
+                islocal = true,
+                decl = { name = "x", type = false } },
+            { _tag = "TopLevel_Var",
+                islocal = false,
+                decl = { name = "y", type = false } },
+        })
     end)
 
     it("can parse toplevel function declarations", function()
-        assert_parse("./testfiles/toplevel_functions.titan")
-    end)
+        local program, err = parse_file("./testfiles/toplevel_functions.titan")
+        assert.truthy(program)
+        assert_ast(program, {
+            { _tag = "TopLevel_Func",
+                islocal = true,
+                name = "fA",
+                params = {},
+                block = { _tag = "Stat_Block", stats = {} } },
+
+            { _tag = "TopLevel_Func",
+                islocal = true,
+                name = "fB",
+                params = {
+                    { _tag = "Decl_Decl", name = "x" },
+                },
+                block = { _tag = "Stat_Block", stats = {} } },
+
+            { _tag = "TopLevel_Func",
+                islocal = true,
+                name = "fC",
+                params = {
+                    { _tag = "Decl_Decl", name = "x" },
+                    { _tag = "Decl_Decl", name = "y" },
+                },
+                block = { _tag = "Stat_Block", stats = {} } },
+        })
+   end)
 
     it("can parse types", function()
-        assert_parse("./testfiles/types.titan")
+        local program, err = parse_file("./testfiles/types.titan")
+        assert.truthy(program)
+        assert_ast(program, {
+            { decl = { type = { _tag = "Type_Basic", name = "nil" } } },
+            { decl = { type = { _tag = "Type_Basic", name = "int" } } },
+            { decl = { type = { _tag = "Type_Array", subtype =
+                                    {_tag = "Type_Basic", name = "int" } } } },
+            { decl = { type = { _tag = "Type_Array", subtype =
+                                { _tag = "Type_Array", subtype =
+                                    {_tag = "Type_Basic", name = "int" } } } } },
+        })
     end)
 
     it("can parse values", function()
-        assert_parse("./testfiles/values.titan")
+        local program, err = parse_file("./testfiles/values.titan")
+        assert.truthy(program)
+        assert_ast(program[1].block.stats, {
+            { exp = { _tag = "Exp_Value", value = nil }},
+            { exp = { _tag = "Exp_Value", value = false }},
+            { exp = { _tag = "Exp_Value", value = true }},
+            { exp = { _tag = "Exp_Value", value = 10}},
+            { exp = { _tag = "Exp_Value", value = "asd" }},
+            { exp = { _tag = "Exp_Var", var = {
+                        _tag = "Var_Name", name = "y" }}},
+            { exp = { _tag = "Exp_Value", value = 1 }},
+        })
     end)
 
     it("can parse table constructors", function()
-        assert_parse("./testfiles/tablecons.titan")
+        local program, error = parse_file("./testfiles/tablecons.titan")
+        assert.truthy(program)
+        assert_ast(program[1].block.stats, {
+            { exp = { _tag = "Exp_Table", exps = {} }},
+            { exp = { _tag = "Exp_Table", exps = {
+                { _tag = "Exp_Value", value = 10 },
+                { _tag = "Exp_Value", value = 20 },
+                { _tag = "Exp_Value", value = 30 }, }}},
+            { exp = { _tag = "Exp_Table", exps = {
+                { _tag = "Exp_Value", value = 40 },
+                { _tag = "Exp_Value", value = 50 },
+                { _tag = "Exp_Value", value = 60 }, }}},
+        })
     end)
 
     it("can parse statements", function()
-        assert_parse("./testfiles/statements.titan")
+        local program, err = parse_file("./testfiles/statements.titan")
+        assert.truthy(program)
+        assert_ast(program[1].block.stats, {
+            { _tag = "Stat_While",
+              condition = { _tag = "Exp_Value" },
+              block = { _tag = "Stat_Block" } },
+            
+            { _tag = "Stat_Repeat",
+              block = { _tag = "Stat_Block" },
+              condition = { _tag = "Exp_Value" }, },
+            
+            { _tag = "Stat_If",
+                thens = {
+                    { _tag = "Then_Then", exp = { value = 10 } },
+                },
+              elsestat = false, },
+
+            { _tag = "Stat_If",
+                thens = {
+                    { _tag = "Then_Then", exp = { value = 20 } },
+                },
+                elsestat = { _tag = "Stat_Block" }, },
+
+
+            { _tag = "Stat_If",
+                thens = {
+                    { _tag = "Then_Then", exp = { value = 30 } },
+                    { _tag = "Then_Then", exp = { value = 40 } },
+                },
+                elsestat = false, },
+
+            { _tag = "Stat_If",
+              thens = {
+                    { _tag = "Then_Then", exp = { value = 50 } },
+                    { _tag = "Then_Then", exp = { value = 60 } },
+                },
+                elsestat = { _tag = "Stat_Block" }, },
+
+            { _tag = "Stat_Block",
+                stats = {
+                    { _tag = "Stat_Decl", decl = { name = "x" } },
+                    { _tag = "Stat_Assign",
+                        var = { name = "x" },
+                        exp = { value = 10 } },
+                    { _tag = "Stat_Assign",
+                        var = { name = "x" },
+                        exp = { value = 11 } },
+                    { _tag = "Stat_Call",
+                        callexp = { _tag = "Exp_Call" } } } },
+
+            { _tag = "Stat_Return", exp = { _tag = "Exp_Var" } },
+        })
     end)
 
     it("can parse return statements", function()
-        assert_parse("./testfiles/return_statements.titan")
+        -- Just check if it succeeds or fails in all the cases.
+        
+        local program, err = parse_file("./testfiles/return_statements.titan")
+        assert.truthy(program)
     end)
 
     it("return statements must be the last in the block", function()
@@ -53,11 +190,94 @@ describe("Titan parser", function()
     end)
 
     it("can parse binary and unary operators", function()
-        assert_parse("./testfiles/operators.titan")
+        local program, err = parse_file("./testfiles/operators.titan")
+        assert.truthy(program)
+
+        assert_ast(program[1].block.stats[1].exp,
+            { op = "or",
+                lhs = { op = "not", exp = { value = 1 } },
+                rhs = { op = "and",
+                    lhs = { op = "and",
+                        lhs = { value = 2 },
+                        rhs = { value = 3 } },
+                    rhs = { value = 4} } }
+        )
+
+        assert_ast(program[2].block.stats[1].exp,
+            { op = "==",
+                lhs = { op = "==",
+                    lhs = { op = "<=",
+                        lhs = { value = 1 },
+                        rhs = { value = 2 } },
+                    rhs = { op = "<",
+                        lhs = { value = 4 },
+                        rhs = { value = 5 } } },
+                rhs = { op = "~=",
+                    lhs = { value = 6 },
+                    rhs = { value = 7 } } }
+        )
+
+        assert_ast(program[3].block.stats[1].exp,
+            { op = "|",
+                lhs = { op = "~",
+                    lhs = { op = "~", exp = { op = "~", exp = { value = 1 } } },
+                    rhs = { op = ">>",
+                        lhs = { op = "<<",
+                            lhs = { value = 2 },
+                            rhs = { value = 3 } } } },
+                rhs = { op = "&",
+                    lhs = { value = 5 },
+                    rhs = { value = 6 } } }
+        )
+
+        assert_ast(program[4].block.stats[1].exp,
+            { op = "..",
+                lhs = { value = "a" },
+                rhs = { op = "..",
+                    lhs = { value = "b" },
+                    rhs = { value = "c" } } }
+        )
+
+        assert_ast(program[5].block.stats[1].exp,
+            { op = "+",
+                lhs = { op = "/",
+                    lhs = { op = "-", exp = { op = "-", exp = { value = 1 } } },
+                    rhs = { value = 2 } },
+                rhs = { op = "*",
+                    lhs = { value = 3 },
+                    rhs = { op = "#", exp = { value = "a" } } } }
+        )
+
+        assert_ast(program[6].block.stats[1].exp,
+            { op = "*",
+                lhs = { op = "-",
+                    exp = { op = "^",
+                        lhs = { value = 1 },
+                        rhs = { op = "-",
+                            exp = { op = "^",
+                                lhs = { value = 2 },
+                                rhs = { value = 3 } } } } },
+                 rhs = { value = 4 } }
+        )
     end)
 
     it("can parse suffix operators", function()
-        assert_parse("./testfiles/suffix.titan")
+        local program, err = parse_file("./testfiles/suffix.titan")
+        assert.truthy(program)
+        assert_ast(program[1].block.stats[1].exp,
+            { _tag = "Exp_Unop", op = "-",
+                exp = { _tag = "Exp_Binop", op = "^",
+                    rhs = { _tag = "Exp_Value", value = 3 },
+                    lhs = { _tag = "Exp_Var", var = {
+                        _tag = "Var_Index",
+                        exp2 = { _tag = "Exp_Value", value = 2 },
+                        exp1 = { _tag = "Exp_Call",
+                            args = { _tag = "Args_Method", method = "foo" },
+                            exp = { _tag = "Exp_Call",
+                                args = { _tag = "Args_Func" },
+                                exp = { _tag = "Exp_Var",
+                                    var = { _tag = "Var_Name", name = "x" }}}}}}}}
+        )
     end)
 
     it("only allows call expressions as statements", function()
@@ -67,7 +287,33 @@ describe("Titan parser", function()
     end)
 
     it("can parse calls without parenthesis", function()
-        assert_parse("./testfiles/string_and_table_calls.titan")
-    end)
+        local program, err =
+            parse_file("./testfiles/string_and_table_calls.titan")
+        assert.truthy(program)
+        assert_ast(program[1].block.stats, {
+            { callexp = {
+                _tag = "Exp_Call", args = {
+                    _tag = "Args_Func", args = { } } } },
+            { callexp = {
+                _tag = "Exp_Call", args = {
+                    _tag = "Args_Func", args = {
+                        { _tag = "Exp_Value", value = "qwe" } } } } },
+            { callexp = {
+                _tag = "Exp_Call", args = {
+                    _tag = "Args_Func", args = {
+                        { _tag = "Exp_Table" } } } } },
 
+            { callexp = {
+                _tag = "Exp_Call", args = {
+                    _tag = "Args_Method", args = { } } } },
+            { callexp = {
+                _tag = "Exp_Call", args = {
+                    _tag = "Args_Method", args = {
+                        { _tag = "Exp_Value", value = "asd" } } } } },
+            { callexp = {
+                _tag = "Exp_Call", args = {
+                    _tag = "Args_Method", args = {
+                        { _tag = "Exp_Table" } } } } },
+        })
+    end)
 end)
