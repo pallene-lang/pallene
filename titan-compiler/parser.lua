878e4590c52c29263a74f7346466e6975694507d
Hugo Musso Gualandi <hgualandi@inf.puc-rio.br>
2017-12-07 17:35:21 -0200

    Update testfiles to current Titan syntax

--- titan-compiler/parser.lua
+++ titan-compiler/parser.lua
@@ -32,6 +32,7 @@ end
 
 function defs.rettypeopt(pos, x)
     if not x then
+        -- When possible, we should change this default to the empty list
         return { defs.Type_Name(pos, "nil") }
     else
         return x
