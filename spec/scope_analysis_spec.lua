c049fbd6efda77bbf9f23e104fc7b7b31d22b2cb
Hugo Musso Gualandi <hgualandi@inf.puc-rio.br>
2018-03-06 14:27:57 -0300

    Create a separate pass for variable scope analysis
    Trying to do scope analysis and typechecking at the same time tried to
    push the code in conflicting directions. For example, consider something
    liks this
    
        local x = 1
        local y = 2
        local x: Point = { x = x, y = y }
    
    In the third line, the scope analysis wants to bind the names in the
    type annotation and initializer expression before adding the new x to
    the scope. On the other hand, the typechecker wants to know the type of
    x before typechecking expr so it can do top down type inference for the
    initializer list.
    
    This first commit creates a separate scope analysis pass and creates a
    test suite for it but does not yet integrate with the rest of the
    compiler. We still need to rewrite the typechecker to use the result of
    this pass instead of duplicating it.

---
+++ spec/scope_analysis_spec.lua
@@ -0,0 +1,254 @@
+local parser = require 'titan-compiler.parser'
+local scope_analysis = require 'titan-compiler.scope_analysis'
+
+local function run_scope_analysis(code)
+    local prog, errs
+    prog = assert(parser.parse("(scope_analysis_spec)", code))
+    prog, errs = scope_analysis.bind_names(prog)
+    return prog, table.concat(errs, "\n")
+end
+
+describe("Scope analysis: ", function()
+
+    it("global variables work", function()
+        local prog, errs = run_scope_analysis([[
+            local x: integer = 10
+            function fn(): integer
+                return x
+            end
+        ]])
+        assert.truthy(prog)
+        assert.are.equal(
+            prog[1], -- x
+            prog[2].block.stats[1].exp.var._decl)
+    end)
+
+    it("function parameters work", function()
+        local prog, errs = run_scope_analysis([[
+            function fn(x: integer): integer
+                return x
+            end
+        ]])
+        assert.truthy(prog)
+        assert.are.equal(
+            prog[1].params[1], -- x
+            prog[1].block.stats[1].exp.var._decl)
+    end)
+
+    it("local variables work", function()
+        local prog, errs = run_scope_analysis([[
+            function fn(): integer
+                local x = 17
+                return x
+            end
+        ]])
+        assert.truthy(prog)
+        assert.are.equal(
+            prog[1].block.stats[1].decl, -- x
+            prog[1].block.stats[2].exp.var._decl)
+    end)
+
+    it("functions can be recursive", function()
+        local prog, errs = run_scope_analysis([[
+            function fac(n: integer): integer
+                if n == 0 then
+                    return 1
+                else
+                    return n * fac(n-1)
+                end
+            end
+        ]])
+        assert.truthy(prog)
+        assert.are.equal(
+            prog[1], -- fac
+            prog[1].block.stats[1].elsestat.stats[1].exp.rhs.exp.var._decl)
+    end)
+
+    it("forbids variables from being used before they are defined", function()
+        local prog, errs = run_scope_analysis([[
+            function fn(): nil
+                x = 17
+                local x = 18
+            end
+        ]])
+        assert.falsy(prog)
+        assert.match("variable 'x' not declared", errs)
+    end)
+
+    it("do-end limits variable scope", function()
+        local prog, errs = run_scope_analysis([[
+            function fn(): nil
+                do
+                    local x = 17
+                end
+                x = 18
+            end
+        ]])
+        assert.falsy(prog)
+        assert.match("variable 'x' not declared", errs)
+    end)
+
+    it("local variable scope doesn't shadow its type annotation", function()
+        local prog, errs = run_scope_analysis([[
+            record x
+                x: integer
+                y: integer
+            end
+
+            function fn(): integer
+                local x: x = { x=1, y=2 }
+                return x.x
+            end
+        ]])
+        assert.truthy(prog)
+        assert.are.equal(
+            prog[1], -- global x
+            prog[2].block.stats[1].decl.type._decl)
+        assert.are.equal(
+            prog[2].block.stats[1].decl, -- local x
+            prog[2].block.stats[2].exp.var.exp.var._decl)
+    end)
+
+    it("local variable scope doesn't shadow its initializer", function()
+        local prog, errs = run_scope_analysis([[
+            local x = 17
+            function fn(): integer
+                local x = x + 1
+                return x
+            end
+        ]])
+        assert.truthy(prog)
+        assert.are.equal(
+            prog[1], -- global x
+            prog[2].block.stats[1].exp.lhs.var._decl)
+        assert.are.equal(
+            prog[2].block.stats[1].decl, -- local x
+            prog[2].block.stats[2].exp.var._decl)
+    end)
+
+    it("repeat-until scope includes the condition", function()
+        local prog, errs = run_scope_analysis([[
+            function fn(): integer
+                local x = 0
+                repeat
+                    x = x + 1
+                    local limit = x * 10
+                until limit >= 100
+                return x
+            end
+        ]])
+        assert.truthy(prog)
+        assert.are.equal(
+            prog[1].block.stats[2].block.stats[2].decl, -- local limit
+            prog[1].block.stats[2].condition.lhs.var._decl)
+    end)
+
+    it("for loop variable scope doesn't shadow its type annotation", function()
+        local prog, errs = run_scope_analysis([[
+            record x
+                x: integer
+                y: integer
+            end
+            function fn(): integer
+                for x: x = 1, 10 do
+                   return x
+                end
+            end
+        ]])
+        assert.truthy(prog)
+        assert.are.equal(
+            prog[1], -- global x
+            prog[2].block.stats[1].decl.type._decl)
+        assert.are.equal(
+            prog[2].block.stats[1].decl, -- local x
+            prog[2].block.stats[1].block.stats[1].exp.var._decl)
+    end)
+
+    it("for loop variable scope doesn't shadow its initializers", function()
+        local prog, errs = run_scope_analysis([[
+            function fn(): integer
+                local x = 10
+                for x: integer = x, x, x do
+                   return x
+                end
+            end
+        ]])
+        assert.truthy(prog)
+        assert.are.equal(
+            prog[1].block.stats[1].decl, -- local x
+            prog[1].block.stats[2].start.var._decl)
+        assert.are.equal(
+            prog[1].block.stats[1].decl,
+            prog[1].block.stats[2].finish.var._decl)
+        assert.are.equal(
+            prog[1].block.stats[1].decl,
+            prog[1].block.stats[2].inc.var._decl)
+        assert.are.equal(
+            prog[1].block.stats[2].decl, -- for x
+            prog[1].block.stats[2].block.stats[1].exp.var._decl)
+    end)
+
+    it("allows mutually-recursive toplevel definitions", function()
+        local prog, errs = run_scope_analysis([[
+            function f(): integer
+                return g() + p.x.n
+            end
+
+            function g(): integer
+                return f() + p.x.n
+            end
+
+            record BoxyPoint
+                x: IntBox
+                y: IntBox
+            end
+
+            record IntBox
+                n: integer
+            end
+
+            local p: BoxyPoint = { x = { n = 1 }, y = { n = 2 } }
+        ]])
+        assert.truthy(prog)
+
+        -- function f
+        assert.are.equal(
+            prog[1],
+            prog[2].block.stats[1].exp.lhs.exp.var._decl)
+
+        -- function g
+        assert.are.equal(
+            prog[2],
+            prog[1].block.stats[1].exp.lhs.exp.var._decl)
+
+        -- record BoxyPoint
+        assert.are.equal(
+            prog[3],
+            prog[5].decl.type._decl)
+
+        -- record IntBox
+        assert.are.equal(
+            prog[4],
+            prog[3].fields[1].type._decl)
+        assert.are.equal(
+            prog[4],
+            prog[3].fields[2].type._decl)
+
+        -- local p
+        assert.are.equal(
+            prog[5],
+            prog[1].block.stats[1].exp.rhs.var.exp.var.exp.var._decl)
+        assert.are.equal(
+            prog[5],
+            prog[2].block.stats[1].exp.rhs.var.exp.var.exp.var._decl)
+    end)
+
+    it("forbids multiple toplevel declarations with the same name", function()
+        local prog, errs = run_scope_analysis([[
+            local x: integer = 10
+            local x: integer = 11
+        ]])
+        assert.falsy(prog)
+        assert.match("duplicate toplevel declaration", errs)
+    end)
+end)
---
+++ titan-compiler/scope_analysis.lua
@@ -0,0 +1,348 @@
+local scope_analysis = {}
+
+local ast = require "titan-compiler.ast"
+local location = require "titan-compiler.location"
+local symtab = require "titan-compiler.symtab"
+local types = require "titan-compiler.types"
+
+local bind_names_program
+local bind_names_type
+local bind_names_toplevel
+local bind_names_decl
+local bind_names_stat
+local bind_names_then
+local bind_names_var
+local bind_names_exp
+local bind_names_args
+local bind_names_field
+
+-- Perform lexycal analysis (name binding) on a Titan module.
+--
+-- Sets a _decl field on Var.Name and Type.Name nodes.
+-- This will need to be revised when we introduce modules, because then the "."
+-- will mean more than one thing (C++'s "." and "::")
+--
+-- @param prog AST for the whole module
+-- @return true, or false followed by a list of compilation errors
+function scope_analysis.bind_names(prog)
+    local errors = {}
+    local st = symtab.new()
+    bind_names_program(prog, st, errors)
+    if #errors == 0 then
+        return prog, errors
+    else
+        return false, errors
+    end
+end
+
+
+--
+-- Local functions
+--
+
+local function scope_error(errors, loc, fmt, ...)
+    local errmsg = location.format_error(loc, fmt, ...)
+    table.insert(errors, errmsg)
+end
+
+-- Return the variable name declared by a given toplevel node
+local function toplevel_name(tlnode)
+    local tag = tlnode._tag
+    if     tag == ast.Toplevel.Func then
+        return tlnode.name
+    elseif tag == ast.Toplevel.Var then
+        return tlnode.decl.name
+    elseif tag == ast.Toplevel.Record then
+        return tlnode.name
+    elseif tag == ast.Toplevel.Import then
+        return tlnode.localname
+    else
+        error("impossible")
+    end
+end
+
+
+--
+-- bind_names
+--
+
+bind_names_program = function(prog, st, errors)
+    st:with_block(function()
+        -- First we put all toplevel names in scope, to allow mutual references.
+        -- We don't allow allow shadowing because it ambiguous with letrec and
+        -- even without letrec it would still interact confusingly with module
+        -- exports.
+        for _, tlnode in ipairs(prog) do
+            local name = toplevel_name(tlnode)
+            local dup = st:find_dup(name)
+            if dup then
+                tlnode._ignore = true
+                scope_error(errors, tlnode.loc,
+                    "duplicate toplevel declaration for %s, previous one at line %d",
+                    name, dup.loc.line)
+            else
+                tlnode._ignore = false
+                st:add_symbol(name, tlnode)
+            end
+        end
+
+        -- Now we can resolve names in toplevel type annotations and in function
+        -- bodies.
+        for _, tlnode in ipairs(prog) do
+            if not tlnode._ignore then
+                bind_names_toplevel(tlnode, st, errors)
+            end
+        end
+    end)
+end
+
+bind_names_type = function(type_node, st, errors)
+    local tag = type_node._tag
+    if tag == ast.Type.Nil or
+            tag == ast.Type.Boolean or
+            tag == ast.Type.Integer or
+            tag == ast.Type.Float or
+            tag == ast.Type.String then
+        -- Nothing to do
+
+    elseif tag == ast.Type.Name then
+        local name = type_node.name
+        type_node._decl = st:find_symbol(name) or false
+        if not type_node._decl then
+            scope_error(errors, type_node.loc, "type '%s' not found", name)
+        end
+
+    elseif tag == ast.Type.Array then
+        bind_names_type(type_node.subtype, st, errors)
+
+    elseif tag == ast.Type.Function then
+        for _, ptype in ipairs(type_node.argtypes) do
+            bind_names_type(ptype, st, errors)
+        end
+        for _, rettype in ipairs(type_node.rettypes) do
+            bind_names_type(rettype, st, errors)
+        end
+
+    else
+        error("impossible")
+    end
+
+end
+
+bind_names_toplevel = function(tlnode, st, errors)
+    local tag = tlnode._tag
+    if     tag == ast.Toplevel.Func then
+        for _, decl in ipairs(tlnode.params) do
+            bind_names_decl(decl, st, errors)
+        end
+        for _, rettype in ipairs(tlnode.rettypes) do
+            bind_names_type(rettype, st, errors)
+        end
+
+        st:with_block(function()
+            for _, decl in ipairs(tlnode.params) do
+                st:add_symbol(decl.name, decl)
+                -- A compile-time error for duplicates here would be nice...
+            end
+            bind_names_stat(tlnode.block, st, errors)
+        end)
+
+    elseif tag == ast.Toplevel.Var then
+        bind_names_decl(tlnode.decl, st, errors)
+            bind_names_exp(tlnode.value, st, errors)
+
+    elseif tag == ast.Toplevel.Record then
+        for _, field_decl in ipairs(tlnode.fields) do
+            bind_names_decl(field_decl, st, errors)
+        end
+
+    elseif tag == ast.Toplevel.Import then
+        scope_error(errors, loc, "import statements are not yet implemented")
+
+    else
+        error("impossible")
+    end
+end
+
+bind_names_decl = function(decl, st, errors)
+    local tag = decl._tag
+    if tag == ast.Decl.Decl then
+        if decl.type then
+            bind_names_type(decl.type, st, errors)
+        end
+    else
+        error("impossible")
+    end
+end
+
+bind_names_stat = function(stat, st, errors)
+    local tag = stat._tag
+    if     tag == ast.Stat.Block then
+        st:with_block(function()
+            for _, inner_stat in ipairs(stat.stats) do
+                bind_names_stat(inner_stat, st, errors)
+            end
+        end)
+
+    elseif tag == ast.Stat.While then
+        bind_names_exp(stat.condition, st, errors)
+        bind_names_stat(stat.block, st, errors)
+
+    elseif tag == ast.Stat.Repeat then
+        assert(stat.block._tag == ast.Stat.Block)
+        st:with_block(function()
+            for _, inner_stat in ipairs(stat.block.stats) do
+                bind_names_stat(inner_stat, st, errors)
+            end
+            bind_names_exp(stat.condition, st, errors)
+        end)
+
+    elseif tag == ast.Stat.If then
+        for _, then_ in ipairs(stat.thens) do
+            bind_names_then(then_, st, errors)
+        end
+        if stat.elsestat then
+            bind_names_stat(stat.elsestat, st, errors)
+        end
+
+    elseif tag == ast.Stat.For then
+        bind_names_decl(stat.decl, st, errors)
+        bind_names_exp(stat.start, st, errors)
+        bind_names_exp(stat.finish, st, errors)
+        if stat.inc then
+            bind_names_exp(stat.inc, st, errors)
+        end
+        st:with_block(function()
+            st:add_symbol(stat.decl.name, stat.decl)
+            bind_names_stat(stat.block, st, errors)
+        end)
+
+    elseif tag == ast.Stat.Assign then
+        bind_names_var(stat.var, st, errors)
+        bind_names_exp(stat.exp, st, errors)
+
+    elseif tag == ast.Stat.Decl then
+        bind_names_decl(stat.decl, st, errors)
+        bind_names_exp(stat.exp, st, errors)
+        st:add_symbol(stat.decl.name, stat.decl)
+
+    elseif tag == ast.Stat.Call then
+        bind_names_exp(stat.exp, st, errors)
+
+    elseif tag == ast.Stat.Return then
+        bind_names_exp(stat.exp, st, errors)
+
+    else
+        error("impossible")
+    end
+end
+
+bind_names_then = function(then_, st, errors)
+    local tag = then_._tag
+    if tag == ast.Then.Then then
+        bind_names_exp(then_.condition, st, errors)
+        bind_names_stat(then_.block, st, errors)
+    else
+        error("impossible")
+    end
+end
+
+bind_names_var = function(var, st, errors)
+    local tag = var._tag
+    if     tag == ast.Var.Name then
+        var._decl = st:find_symbol(var.name) or false
+        if not var._decl then
+            scope_error(errors, var.loc,
+                "variable '%s' not declared", var.name)
+        end
+
+    elseif tag == ast.Var.Bracket then
+        bind_names_exp(var.exp1, st, errors)
+        bind_names_exp(var.exp2, st, errors)
+
+    elseif tag == ast.Var.Dot then
+        bind_names_exp(var.exp, st, errors)
+
+    else
+        error("impossible")
+    end
+end
+
+bind_names_exp = function(exp, st, errors)
+    local tag = exp._tag
+    if     tag == ast.Exp.Nil then
+        -- Nothing to do
+
+    elseif tag == ast.Exp.Bool then
+        -- Nothing to do
+
+    elseif tag == ast.Exp.Integer then
+        -- Nothing to do
+
+    elseif tag == ast.Exp.Float then
+        -- Nothing to do
+
+    elseif tag == ast.Exp.String then
+        -- Nothing to do
+
+    elseif tag == ast.Exp.Initlist then
+        for _, field in ipairs(exp.fields) do
+            bind_names_field(field, st, errors)
+        end
+
+    elseif tag == ast.Exp.Call then
+        bind_names_exp(exp.exp, st, errors)
+        bind_names_args(exp.args, st, errors)
+
+    elseif tag == ast.Exp.Var then
+        bind_names_var(exp.var, st, errors)
+
+    elseif tag == ast.Exp.Unop then
+        bind_names_exp(exp.exp, st, errors)
+
+    elseif tag == ast.Exp.Concat then
+        for _, inner_exp in ipairs(exp.exps) do
+            bind_names_exp(inner_exp, st, errors)
+        end
+
+    elseif tag == ast.Exp.Binop then
+        bind_names_exp(exp.lhs, st, errors)
+        bind_names_exp(exp.rhs, st, errors)
+
+    elseif tag == ast.Exp.Cast then
+        bind_names_exp(exp.exp, st, errors)
+        bind_names_type(exp.target, st, errors)
+
+    else
+        error("impossible")
+    end
+end
+
+bind_names_args = function(args, st, errors)
+    local tag = args._tag
+    if     tag == ast.Args.Func then
+        for _, exp in ipairs(args.args) do
+            bind_names_exp(exp, st, errors)
+        end
+
+    elseif tag == ast.Args.Method then
+        for _, exp in ipairs(args.args) do
+            bind_names_exp(exp, st, errors)
+        end
+
+    else
+        error("impossible")
+    end
+end
+
+bind_names_field = function(field, st, errors)
+    local tag = field._tag
+    if tag == ast.Field.Field then
+        bind_names_exp(field.exp, st, errors)
+    else
+        error("impossible")
+    end
+end
+
+
+return scope_analysis
