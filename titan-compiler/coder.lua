cf8db7660b1b2340e7b325a71ff87c697127788a
Hugo Musso Gualandi <hgualandi@inf.puc-rio.br>
2018-03-13 18:06:28 -0300

    Implement Lua entry point.
    Titan entry point is still hardcoded

--- titan-compiler/coder.lua
+++ titan-compiler/coder.lua
@@ -25,10 +25,13 @@ local whole_file_template = [[
 #include "lualib.h"
 
 #include "lapi.h"
+#include "lfunc.h"
 #include "lgc.h"
 #include "lobject.h"
 #include "lstate.h"
 #include "ltable.h"
+
+${DEFINE_FUNCTIONS}
 
 int luaopen_${MODNAME}(lua_State *L)
 {
@@ -78,11 +81,61 @@ local function c_float(n)
     return string.format("%f", n)
 end
 
+local function c_declaration(ctyp, varname)
+    -- This simple concatenation won't work with function pointers
+    return ctyp .. " " .. varname
+end
+
 --
 --
 --
 
-local function set_slot(typ, slot_address, value)
+-- This name-mangling scheme is designed to avoid clashes between the function
+-- names created in separate models.
+local function mangle_function_name(modname, funcname, suffix)
+    return string.format("ttt_%d%s%d%s%s",
+        #modname, modname, #funcname, funcname, suffix)
+end
+
+local function local_name(varname)
+    return string.format("local_%s", varname)
+end
+
+-- @param type Type of the titan value
+-- @returns type of the corresponding C variable
+local function ctype(typ)
+    local tag = typ._tag
+    if     tag == types.T.Nil      then return "int"
+    elseif tag == types.T.Boolean  then return "int"
+    elseif tag == types.T.Integer  then return "lua_Integer"
+    elseif tag == types.T.Float    then return "lua_Number"
+    elseif tag == types.T.String   then return "TString *"
+    elseif tag == types.T.Function then error("not implemented yet")
+    elseif tag == types.T.Array    then return "Table *"
+    elseif tag == types.T.Record   then error("not implemented yet")
+    else error("impossible")
+    end
+end
+
+
+local function get_slot(typ, src_slot_address)
+    local tmpl
+    local tag = typ._tag
+    if     tag == types.T.Nil      then tmpl = "0"
+    elseif tag == types.T.Boolean  then tmpl = "bvalue(${SRC})"
+    elseif tag == types.T.Integer  then tmpl = "ivalue(${SRC})"
+    elseif tag == types.T.Float    then tmpl = "fltvalue(${SRC})"
+    elseif tag == types.T.String   then tmpl = "tsvalue(${SRC})"
+    elseif tag == types.T.Function then error("not implemented")
+    elseif tag == types.T.Array    then tmpl = "hvalue(${SRC})"
+    elseif tag == types.T.Record   then error("not implemented")
+    else error("impossible")
+    end
+    return util.render(tmpl, {SRC = src_slot_address})
+end
+
+
+local function set_slot(typ, dst_slot_address, value)
     local tmpl
     local tag = typ._tag
     if     tag == types.T.Nil      then tmpl = "setnilvalue(${DST});"
@@ -95,7 +148,7 @@ local function set_slot(typ, slot_address, value)
     elseif tag == types.T.Record   then error("not implemented yet")
     else error("impossible")
     end
-    return util.render(tmpl, { DST = slot_address, SRC = value })
+    return util.render(tmpl, { DST = dst_slot_address, SRC = value })
 end
 
 local function toplevel_is_value_declaration(tl_node)
@@ -126,6 +179,87 @@ generate_program = function(prog, modname)
         end
     end
 
+    -- Create toplevel function declarations
+    local define_functions
+    do
+        local function_definitions = {}
+        for _, tl_node in ipairs(prog) do
+            if tl_node._tag == ast.Toplevel.Func then
+                -- Titan entry point
+                local titan_entry_point_name =
+                    mangle_function_name(modname, tl_node.name, "_titan")
+
+                assert(#tl_node._type.rettypes == 1)
+                local ret_ctype = ctype(tl_node._type.rettypes[1])
+
+                local args = {}
+                table.insert(args, [[lua_State * L]])
+                table.insert(args, [[Table * titan_globals]])
+                for _, param in ipairs(tl_node.params) do
+                    local name = param.name
+                    local typ  = param._type
+                    table.insert(args,
+                        c_declaration(ctype(typ), local_name(name)))
+                end
+
+                table.insert(function_definitions,
+                    util.render([[
+                        ${RET} ${NAME}(${ARGS})
+                        {
+                            ${BODY}
+                        }
+                    ]], {
+                        RET = ret_ctype,
+                        NAME = titan_entry_point_name,
+                        ARGS = table.concat(args, ", "),
+                        BODY = [[/*TODO*/ return 42;]]
+                    })
+                )
+
+                -- Lua entry point
+                local lua_entry_point_name =
+                    mangle_function_name(modname, tl_node.name, "_lua")
+
+                assert(#tl_node._type.rettypes == 1)
+                local ret_typ = tl_node._type.rettypes[1]
+
+                local args = {}
+                table.insert(args, [[L]])
+                table.insert(args, [[hvalue(&clCvalue(stack_func)->upvalue[1])]])
+                for i, param in ipairs(tl_node.params) do
+                    local slot = util.render([[stack_func + ${I}]], {
+                        I = c_integer(i)
+                    })
+                    table.insert(args, get_slot(param._type, slot))
+                end
+
+                table.insert(function_definitions,
+                    util.render([[
+                        int ${LUA_ENTRY_POINT}(lua_State *L)
+                        {
+                            StkId stack_func = L->ci->func;
+                            ${RET_DECL} = ${TITAN_ENTRY_POINT}(${ARGS});
+                            ${SET_RET}
+                            api_incr_top(L);
+                            return 1;
+                        }
+                    ]], {
+                        LUA_ENTRY_POINT = lua_entry_point_name,
+                        TITAN_ENTRY_POINT = titan_entry_point_name,
+                        RET_DECL = c_declaration(ctype(ret_typ), "ret"),
+                        ARGS = table.concat(args, ", "),
+                        SET_RET = set_slot(ret_typ, "L->top", "ret"),
+                    })
+                )
+
+                --
+                tl_node._titan_entry_point = titan_entry_point_name
+                tl_node._lua_entry_point = lua_entry_point_name
+            end
+        end
+        define_functions = table.concat(function_definitions, "\n")
+    end
+
     -- Construct the values in the toplevel
     local initialize_toplevel
     do
@@ -144,20 +278,31 @@ generate_program = function(prog, modname)
 
         for _, tl_node in ipairs(prog) do
             if tl_node._global_index then
+                local arr_slot = util.render([[ &titan_globals->array[${I}] ]], {
+                    I = c_integer(tl_node._global_index)
+                })
+
                 local tag = tl_node._tag
                 if     tag == ast.Toplevel.Func then
-                    error("not implemented yet")
+                    table.insert(parts,
+                        util.render([[
+                            {
+                                CClosure *func = luaF_newCclosure(L, 2);
+                                func->f = ${LUA_ENTRY_POINT};
+                                setpvalue(&func->upvalue[0], ${TITAN_ENTRY_POINT});
+                                sethvalue(L, &func->upvalue[1], titan_globals);
+                                setclCvalue(L, ${ARR_SLOT}, func);
+                            }
+                        ]],{
+                            LUA_ENTRY_POINT = tl_node._lua_entry_point,
+                            TITAN_ENTRY_POINT = tl_node._titan_entry_point,
+                            ARR_SLOT = arr_slot,
+                        })
+                    )
 
                 elseif tag == ast.Toplevel.Var then
                     local exp = tl_node.value
                     local cstats, cvalue = generate_exp(exp)
-
-                    table.insert(parts, cstats)
-
-                    local arr_slot = util.render([[ &titan_globals->array[${I}] ]], {
-                        I = c_integer(tl_node._global_index)
-                    })
-
                     table.insert(parts, cstats)
                     table.insert(parts, set_slot(exp._type, arr_slot, cvalue))
                 else
@@ -203,6 +348,7 @@ generate_program = function(prog, modname)
 
     local code = util.render(whole_file_template, {
         MODNAME = modname,
+        DEFINE_FUNCTIONS = define_functions,
         INITIALIZE_TOPLEVEL = initialize_toplevel,
         CREATE_MODULE_TABLE = create_module_table,
     })
