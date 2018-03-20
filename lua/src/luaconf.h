6ff7a29b20dd077d787633c07fab3c786bb7b5bf
Hugo Musso Gualandi <hgualandi@inf.puc-rio.br>
2018-03-20 18:06:17 -0300

    Patch Lua5.4
    Make all the internal functions visible to regular C modules (and Titan)
    
    This does not include the Makefile patches that we had done on the
    previous version to add a Titan folder to the package path.

--- lua/src/luaconf.h
+++ lua/src/luaconf.h
@@ -273,12 +273,7 @@
 ** give a warning about it. To avoid these warnings, change to the
 ** default definition.
 */
-#if defined(__GNUC__) && ((__GNUC__*100 + __GNUC_MINOR__) >= 302) && \
-    defined(__ELF__)		/* { */
-#define LUAI_FUNC	__attribute__((visibility("hidden"))) extern
-#else				/* }{ */
-#define LUAI_FUNC	extern
-#endif				/* } */
+#define LUAI_FUNC	LUA_API    /* Titan exports everything */
 
 #define LUAI_DDEC	LUAI_FUNC
 #define LUAI_DDEF	/* empty */
