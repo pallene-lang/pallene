d422012657039408279b5caeaa15d235431a456a
Hugo Musso Gualandi <hgualandi@inf.puc-rio.br>
2018-04-11 16:37:07 -0300

    Fixes from code review

--- benchmarks/matmulsum/main.lua
+++ benchmarks/matmulsum/main.lua
@@ -1,11 +1,5 @@
 local matmul = require(arg[1])
 
-function print_mat(A)
-    for i = 1, #A do
-        print(table.concat(A[i], " "))
-    end
-end
-
 local N = 600
 
 local BIG1 = {}
@@ -20,5 +14,3 @@ BIG2 = BIG1
 
 local s = matmul.matmul(BIG1, BIG2)
 print(s)
-
-
