27c4f67fea407551a2615fce4e8d21d4f30d2d83
Hugo Musso Gualandi <hgualandi@inf.puc-rio.br>
2018-04-28 16:26:58 -0300

    Fixes from code review

--- spec/coder_test_operators.lua
+++ spec/coder_test_operators.lua
@@ -1,12 +1,14 @@
 -- Used in checker_spec
 
+local coder_test_operators = {}
+
 local values = {
     ["boolean"] = { true, false },
     ["integer"] = {
         math.mininteger, -10, -3, -2, -1, 0, 1, 2, 3, 10, math.maxinteger,
     },
     ["float"] = {
-        -math.huge, -2.0, -1.0, 0.0, 1.0, math.pi
+        -math.huge, -2.0, -1.0, 0.0, 1.0, math.pi, math.huge,
     }
 }
 
@@ -18,24 +20,24 @@ local function are_same(a, b)
     return (a == b) or (isnan(a) and isnan(b))
 end
 
-local function check(f_lua, f_titan, args)
-    local ok1, a = pcall(f_lua, table.unpack(args))
-    local ok2, b = pcall(f_titan, table.unpack(args))
-    if ok1 ~= ok2 then
+local function check(f_lua, f_titan, ...)
+    local ok_lua,   r_lua   = pcall(f_lua, ...)
+    local ok_titan, b_titan = pcall(f_titan, ...)
+    if ok_lua ~= ok_titan then
         return false, string.format("lua %s but titan %s",
             (ok1 and "didn't crash" or "crashed"),
             (ok2 and "didn't crash" or "crashed"))
     end
-    if ok1 and ok2 and not are_same(a, b) then
+    if ok_lua and ok_titan and not are_same(a, b) then
         return false, string.format("(lua: %s, titan: %s)",
             tostring(a), tostring(b))
     end
     return true
 end
 
-local function check_unop(op_str, f_lua, f_titan, typ1)
+function coder_test_operators.check_unop(op_str, f_lua, f_titan, typ1)
     for _, x in ipairs(values[typ1]) do
-        local ok, err = check(f_lua, f_titan, {x})
+        local ok, err = check(f_lua, f_titan, x)
         if not ok then
             error(string.format("%s %s: %s",
                 op_str, tostring(x), err))
@@ -43,10 +45,10 @@ local function check_unop(op_str, f_lua, f_titan, typ1)
     end
 end
 
-local function check_binop(op_str, f_lua, f_titan, typ1, typ2)
+function coder_test_operators.check_binop(op_str, f_lua, f_titan, typ1, typ2)
     for _, x in ipairs(values[typ1]) do
         for _, y in ipairs(values[typ2]) do
-            local ok, err = check(f_lua, f_titan, {x, y})
+            local ok, err = check(f_lua, f_titan, x, y)
             if not ok then
                 error(string.format("%s %s %s: %s",
                     tostring(x), op_str, tostring(y), err))
@@ -55,7 +57,4 @@ local function check_binop(op_str, f_lua, f_titan, typ1, typ2)
     end
 end
 
-return {
-    check_unop = check_unop,
-    check_binop = check_binop,
-}
+return coder_test_operators
