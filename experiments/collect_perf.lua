6fcef018b5d6e3e67899b7ff673c61d4d1ddd8cd
Hugo Musso Gualandi <hgualandi@inf.puc-rio.br>
2019-01-23 18:42:18 -0500

    Longer experiments, less outliers...

--- experiments/collect_perf.lua
+++ experiments/collect_perf.lua
@@ -7,14 +7,14 @@ local nrep = tonumber(arg[1]) or 1
 
 local bench = "matmul"
 
-print("N,M,Implementation,Time,IPC,llc_miss_pct")
-for __ = 1, nrep do
-    for _, NM in ipairs({
-        { 100, 1024 },
-        { 200,  128 },
-        { 400,   16 },
-        { 800,    2 },
-    }) do
+print("N,M,Implementation,Time,IPC,llc_miss_pct,Seq")
+for _, NM in ipairs({
+    { 100, 1024 },
+    { 200,  128 },
+    { 400,   16 },
+    { 800,    2 },
+}) do
+    for i = 1, nrep do
         local N = NM[1]
         local M = NM[2]
 
@@ -23,7 +23,7 @@ for __ = 1, nrep do
             "luajit",
         }) do
             local data = benchlib.run_with_impl_name("perf", bench, impl, NM)
-            print(table.concat({N, M,  impl, data.time, data.IPC, data.llc_miss_pct}, ","))
+            print(table.concat({N, M,  impl, data.time, data.IPC, data.llc_miss_pct, i}, ","))
         end
     end
 end
--- experiments/collect_times.lua
+++ experiments/collect_times.lua
@@ -17,29 +17,30 @@ for i = 1, #params do
     quoted_params[i] = util.shell_quote(params[i])
 end
 
-print("Benchmark,Implementation,Time")
-for _ = 1, nrep do
-    -- Lua/Pallene benchmarks
-    for _, impl in ipairs({
+print("Benchmark,Implementation,Time,Seq")
+for _, impl in ipairs({
         "pallene",
         "lua",
         "luajit",
         "capi",
-        "nocheck",
-    }) do
+        "nocheck",}) do
+    for i = 1, nrep do
         local data = benchlib.run_with_impl_name("time", bench, impl, params)
-        print(table.concat({bench, impl, data.time}, ","))
+        print(table.concat({bench, impl, data.time, i}, ","))
     end
+end
 
-    -- C benchmarks
-    -- This code duplication is a bit annoying. In the long run we should get
-    -- rid of these C benchmarks or have a more unified organization for these
-    -- benchmarks...
+-- Lua/Pallene benchmarks
+-- C benchmarks
+-- This code duplication is a bit annoying. In the long run we should get
+-- rid of these C benchmarks or have a more unified organization for these
+-- benchmarks...
+for i = 1, nrep do
     os.execute("cd cbenchmarks; make -q")
     local mode = benchlib.modes.time
     local cmd = string.format("./cbenchmarks/%s %s",
         util.shell_quote(bench), table.concat(quoted_params, " "))
     local data = mode.parse(mode.run(cmd))
-    print(table.concat({bench, "purec", data.time}, ","))
+    print(table.concat({bench, "purec", data.time, i}, ","))
 
 end
