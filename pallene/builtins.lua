685f345ae3ed2aa98ee20dbed0c433efd52c2e28
Hugo Musso Gualandi <Hugo Musso Gualandi>
2019-05-22 22:04:37 -0300

    Suggestions from PR #54

--- pallene/builtins.lua
+++ pallene/builtins.lua
@@ -2,23 +2,16 @@ local ast = require "pallene.ast"
 local location = require "pallene.location"
 local types = require "pallene.types"
 
-
 local builtins = {}
 
--- I don't expect this to ever be shown to the end user
-local loc = location.new("(builtin)", 0,0)
+for _, lua_name in ipairs({"io.write", "table.insert", "table.remove"}) do
+    local pallene_name = string.gsub(lua_name, "%.", "_")
+    local loc = location.new("(builtin)", 0,0) -- (never shown to user)
 
---
--- Builtin functions
---
+    local obj = ast.Toplevel.Builtin(loc, lua_name)
+    obj._type = types.T.Builtin(obj)
 
-builtins.io_write = ast.Toplevel.Builtin(loc, "io.write")
-builtins.io_write._type = types.T.Builtin(builtins.io_write)
-
-builtins.table_insert = ast.Toplevel.Builtin(loc, "table.insert")
-builtins.table_insert._type = types.T.Builtin(builtins.table_insert)
-
-builtins.table_remove = ast.Toplevel.Builtin(loc, "table.remove")
-builtins.table_remove._type = types.T.Builtin(builtins.table_remove)
+    builtins[pallene_name] =  obj
+end
 
 return builtins
