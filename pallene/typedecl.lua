fe46175567cba32c7c0a51b1f35e7675f7c48e44
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2019-06-15 12:05:19 -0300

    Ensure that constructor tag names are unique

--- pallene/typedecl.lua
+++ pallene/typedecl.lua
@@ -2,6 +2,32 @@ local typedecl = {}
 
 local names = setmetatable({}, {__mode = "k"})
 
+-- Unique tag names:
+--
+-- We keep track of all the type tags that we define, so that no two
+-- constructors attempt to use the same type tag.
+
+local existing_tags = {}
+
+local function is_valid_name_component(s)
+    -- In particular, this rules out the separator character "."
+    return string.match(s, "[A-Za-z_][A-Za-z_0-9]*")
+end
+
+local function make_name(mod_name, type_name, cons_name)
+    assert(is_valid_name_component(mod_name))
+    assert(is_valid_name_component(type_name))
+    assert(is_valid_name_component(cons_name))
+    local tag = mod_name .. "." .. type_name .. "." .. cons_name
+    if existing_tags[tag] then
+        error("tag name " .. tag .. " is already being used")
+    else
+        existing_tags[tag] = true
+    end
+    return tag
+end
+
+
 -- Create a properly-namespaced algebraic datatype. Objects belonging to this
 -- type can be pattern matched by inspecting their _tag field. See ast.lua and
 -- types.lua for usage examples.
@@ -25,7 +51,7 @@ function typedecl.declare(module, mod_name, type_name, constructors)
             return node
         end
         module[type_name][cons_name] = cons
-        names[cons] = mod_name .. "." .. type_name .. "." .. cons_name
+        names[cons] = make_name(mod_name, type_name, cons_name)
     end
 end
 
---
+++ spec/typedecl_spec.lua
@@ -0,0 +1,11 @@
+local typedecl = require "pallene.typedecl"
+
+describe("Typedecl", function()
+    it("forbids repeated tags", function()
+        assert.has_error(function()
+            local mod = {}
+            typedecl.declare(mod, "TESTTYPE", "Foo", { Bar = {"x"} })
+            typedecl.declare(mod, "TESTTYPE", "Foo", { Bar = {"x"} })
+        end, "tag name TESTTYPE.Foo.Bar is already being used")
+    end)
+end)
