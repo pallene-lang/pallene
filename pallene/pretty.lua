6eb242761b088e120d03f0f7eb5fbfbcc60a2a96
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2019-06-27 14:19:03 -0300

    Make the C code formatter a bit more clever
    Now it can correctly deal with some things that it could not before such as
    comments after an open brace, or C code before a closing paren.
    
    Because of this, I think we can remove some of the restrictions from the
    style guide.

--- pallene/pretty.lua
+++ pallene/pretty.lua
@@ -1,45 +1,16 @@
 local pretty = {}
 
--- This pretty printer allows us generate readable C code without needing to
--- worry about indenting it as we generate it.
+-- This function reformats a string corresponding to a C source file. It allows
+-- us to produce readable C output without having to worry about indentation
+-- while we are generating it.
 --
--- To keep the implementation simple, we assume that the input C code conforms
--- to our style guide for generated C code, which is described here. The
--- important bit is ensuring that our heuristics can correctly identify when to
--- indent and unindent based solely on the position of {} and () characters in
--- the input program.
+-- The algorithm is not very clever, so you must follow some rules if you want
+-- to get good-looking results:
 --
--- COMMENTS:
---     - Prefer /**/-style comments over //-style comments
---     - Never place inline comments just after a {
---     - Comments should not span multiple lines
---     - Don't put user-generated text inside comments
---     - Don't put C code inside comments
+--   * Use braces on if statements, while loops, and for loops.
+--   * /**/-style comments must not span multiple lines
+--   * Be careful about special characters inside strings and comments
 --
---         /* This is */
---         /* allowed */
---         if (x == y) {
---         }
---
---         if (x == y) { /* This is very bad */
---         }
---
---         /* Don't
---          * do this */
---
--- INDENTATION
---     - Use K&R-style indentation
---     - Multi-line statements must use braces
---
---         /* OK */
---         if (x == y) {
---             blah();
---         }
---
---         /* BAD */
---         if (x == y)
---             blah();
-
 function pretty.reindent_c(input)
     local out = {}
     local indent = 0
@@ -47,31 +18,46 @@ function pretty.reindent_c(input)
     for line in input:gmatch("([^\n]*)") do
         line = line:match("^[ \t]*(.-)[ \t]*$")
 
+        -- Inside functions ignore all empty lines. In the toplevel, collapse
+        -- groups of empty lines between declarations into a single empty line.
         local is_blank = (#line == 0)
-        local should_print =
-            (not is_blank) or
-            (not previous_is_blank and indent == 0)
+        if (not is_blank) or (indent == 0 and not previous_is_blank) then
 
-        local is_preprocessor_directive = (line:match("^#"))
+            local indent_for_this_line
+            if line:match("^#") then
+                -- Preprocessor directives are never indented
+                indent_for_this_line = 0
+            else
+                -- Otherwise, count braces and parens
+                local without_strings = line:gsub('\\\"', "")
+                                            :gsub('".-"', "")
+                local without_comments = without_strings:gsub("/%*.-%*/", "")
+                                                        :gsub("//.*", "")
+                local n_open  = #string.gsub(without_comments, "[^{(]", "")
+                local n_close = #string.gsub(without_comments, "[^})]", "")
+                local unindent_this_line = string.match(line, "^[})]")
 
-        if line:match("^[})]") then
-            indent = indent - 1
-            -- Don't let the indentation level get negative. This confines the
-            -- messed-up indentation to a single function in cases where our
-            -- heuristics have failed to spot the "{" that matches this "}"
-            if indent < 0 then indent = 0 end
-        end
-        if should_print then
-            if not is_preprocessor_directive then
-                table.insert(out, string.rep("    ", indent))
+                indent_for_this_line = indent - (unindent_this_line and 1 or 0)
+
+                if     n_open > n_close then
+                    indent = indent + 1
+                elseif n_open < n_close then
+                    indent = indent - 1
+                end
+
+                if indent < 0 then
+                    -- Don't let the indentation level get negative. If by any
+                    -- chance our heuristics fail to spot an open brace or
+                    -- paren, this confines the messed up indentation to a
+                    -- single function.
+                    indent = 0
+                end
             end
+
+            table.insert(out, string.rep("    ", indent_for_this_line))
             table.insert(out, line)
             table.insert(out, "\n")
         end
-        if line:match("[{(]$") then
-            indent = indent + 1
-        end
-
         previous_is_blank = is_blank
     end
     return table.concat(out)
