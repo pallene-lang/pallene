b316348635ebaf3da15737d10826bb24ea808836
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2019-12-03 18:59:33 -0300

    Implement Mandelbrot benchmarks
    Add the mandelbrot benchmark from the benchmarks game.
    
    Renamed the old mandelbrot benchmark to "objmandelbrot"

--- benchmarks/mandelbrot/main.lua
+++ benchmarks/mandelbrot/main.lua
@@ -1,33 +1,16 @@
-local Complex = require(arg[1])
-local N       = tonumber(arg[2]) or 256
+-- Mandelbrot benchmark from benchmarks game
+-- https://benchmarksgame-team.pages.debian.net/benchmarksgame/description/mandelbrot.html
+--
+-- Translated from the Java version found at
+-- https://benchmarksgame-team.pages.debian.net/benchmarksgame/program/mandelbrot-java-1.html
+--
+--  * I don't use any explicit buffering. In my experiments it speeds up the
+--    program by less than 10%, while considerably increasing the complexity.
+--  * The LuaJIT version needs to be separate, due to the lack of bitwise ops.
 
-local function level(x, y)
-    local c = Complex.new(x, y)
-    local z = Complex.clone(c)
-    local l = 0
-    repeat
-        z = Complex.add(Complex.mul(z, z), c)
-        l = l + 1
-    until Complex.norm2(z) > 4.0 or l > 255
-    return l - 1
-end
+local mandelbrot = require(arg[1])
+local N   = tonumber(arg[2]) or 100
+--local REP = tonumber(arg[3]) or 1
 
-local xmin = -2.0
-local xmax = 2.0
-local ymin = -2.0
-local ymax = 2.0
-
-local dx = (xmax - xmin) / N
-local dy = (ymax - ymin) / N
-
-print("P2")
-print(N, N, 255)
-
-for i = 1, N do
-    local x = xmin + (i - 1) * dx
-    for j = 1, N do
-        local y = ymin + (j - 1) * dy
-        print(level(x, y))
-    end
-end
-
+io.write(string.format("P4\n%d %d\n", N, N))
+mandelbrot.mandelbrot(N)
---
+++ benchmarks/objmandelbrot/lua.lua
@@ -0,0 +1,33 @@
+local function new(x, y)
+    return { x, y }
+end
+
+local function clone(x)
+    return new(x[1], x[2])
+end
+
+local function conj(x)
+    return new(x[1], -x[2])
+end
+
+local function add(x,y)
+    return new(x[1] + y[1], x[2] + y[2])
+end
+
+local function mul(x,y)
+    return new(x[1] * y[1] - x[2] * y[2], x[1] * y[2] + x[2] * y[1])
+end
+
+local function norm2(x)
+    local n = mul(x, conj(x))
+    return n[1]
+end
+
+return {
+    new = new,
+    clone = clone,
+    conj = conj,
+    add = add,
+    mul = mul,
+    norm2 = norm2,
+}
