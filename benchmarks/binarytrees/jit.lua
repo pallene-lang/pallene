f4cbf149209909b1d7f59560e3fd1e51b4702621
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2019-12-10 18:34:39 -0300

    Binary trees benchmark

---
+++ benchmarks/binarytrees/jit.lua
@@ -0,0 +1,34 @@
+local function BottomUpTree(depth)
+    if depth > 0 then
+        depth = depth - 1
+        local left  = BottomUpTree(depth)
+        local right = BottomUpTree(depth)
+        return { left, right }
+    else
+        return { false, false }
+    end
+end
+
+local function ItemCheck(tree)
+    if tree[1] then
+        return 1 + ItemCheck(tree[1]) + ItemCheck(tree[2])
+    else
+        return 1
+    end
+end
+
+local function Stress(mindepth, maxdepth, depth)
+    local iterations = 2 ^ (maxdepth - depth + mindepth)
+    local check = 0
+    for _ = 1, iterations do
+        local t = BottomUpTree(depth)
+        check = check + ItemCheck(t)
+    end
+    return { iterations, check }
+end
+
+return {
+    BottomUpTree = BottomUpTree,
+    ItemCheck = ItemCheck,
+    Stress = Stress,
+}
