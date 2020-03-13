c8143b7395dc8fea47e2c31d9a4966a325bc72d9
Samuel Rowe <samuelrowe1999@gmail.com>
2020-03-13 05:11:28 +0000

    Added copyright notice to all the examples.

--- examples/arithmetic/main.lua
+++ examples/arithmetic/main.lua
@@ -1,3 +1,8 @@
+-- Copyright (c) 2020, The Pallene Developers
+-- Pallene is licensed under the MIT license.
+-- Please refer to the LICENSE and AUTHORS files for details
+-- SPDX-License-Identifier: MIT
+
 arithmetic = require "examples.arithmetic.arithmetic"
 
 print("1 + 2 = " .. arithmetic.add(1, 2))
--- examples/rectangle/main.lua
+++ examples/rectangle/main.lua
@@ -1,3 +1,8 @@
+-- Copyright (c) 2020, The Pallene Developers
+-- Pallene is licensed under the MIT license.
+-- Please refer to the LICENSE and AUTHORS files for details
+-- SPDX-License-Identifier: MIT
+
 m = require "examples.rectangle.rectangle"
 local r = { width = 10.5, height = 5.0 }
 result = m.find_area(r)
