938c72da0c02be2f503a711942b1a16def16e54a
Samuel Rowe <samuelrowe1999@gmail.com>
2020-03-13 05:00:15 +0000

    Moved the examples into their own directories.

--- examples/arithmetic/main.lua
+++ examples/arithmetic/main.lua
@@ -1,4 +1,4 @@
-arithmetic = require "examples.arithmetic"
+arithmetic = require "examples.arithmetic.arithmetic"
 
 print("1 + 2 = " .. arithmetic.add(1, 2))
 print("1.5 - 3.25 = " .. arithmetic.subtract(1.5, 3.25))
--- examples/sum_of_array_main.lua
+++
@@ -1,3 +0,0 @@
-sum_of_array = require "examples.sum_of_array"
-result = sum_of_array.sum({ 5.3, 5.19 })
-print(result)
