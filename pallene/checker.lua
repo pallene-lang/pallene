5d153127e6d62ed19b754eed0891c0079276816c
Samuel Rowe <samuelrowe1999@gmail.com>
2020-06-03 12:16:15 +0530

    Added builtin modules and extended the checker to accomodate them.

--- pallene/checker.lua
+++ pallene/checker.lua
@@ -11,6 +11,7 @@ local symtab = require "pallene.symtab"
 local types = require "pallene.types"
 local typedecl = require "pallene.typedecl"
 local util = require "pallene.util"
+local builtin_modules = require "pallene.builtin_modules"
 
 local checker = {}
 
@@ -62,17 +63,17 @@ end
 -- not need to know how it works. You just need to know that calling
 -- `scope_error()` or `type_error()` will exit the type checking routine
 -- and report a Pallene compilation error.
-local function checker_error(loc, format, ...)
-    local error_message = location.format_error(loc, format, ...)
+local function checker_error(error_location, format, ...)
+    local error_message = location.format_error(error_location, format, ...)
     coroutine.yield(error_message)
 end
 
-local function scope_error(location, format, ...)
-    return checker_error(location, ("scope error: " .. format), ...)
+local function scope_error(error_location, format, ...)
+    return checker_error(error_location, ("scope error: " .. format), ...)
 end
 
-local function type_error(location, format, ...)
-    return checker_error(location, ("type error: " .. format), ...)
+local function type_error(error_location, format, ...)
+    return checker_error(error_location, ("type error: " .. format), ...)
 end
 
 local function check_type_is_condition(exp, format, ...)
@@ -94,11 +95,12 @@ local function declare_type(type_name, cons)
 end
 
 declare_type("Name", {
-    Type     = {"typ"},
-    Local    = {"id"},
-    Global   = {"id"},
-    Function = {"id"},
-    Builtin  = {"name"},
+    Type     = { "typ" },
+    Local    = { "id" },
+    Global   = { "id" },
+    Function = { "id" },
+    Builtin  = { "name" },
+    Module   = { "name" }
 })
 
 --
@@ -136,6 +138,9 @@ function Checker:add_builtin(name)
     self.symbol_table:add_symbol(name, checker.Name.Builtin(name))
 end
 
+function Checker:add_module(name)
+    self.symbol_table:add_symbol(name, checker.Name.Module(name))
+end
 
 function Checker:from_ast_type(ast_typ)
     local tag = ast_typ._tag
@@ -242,15 +247,15 @@ function Checker:check_program(prog_ast)
         local names = {}
         for _, top_level_node in ipairs(prog_ast) do
             local top_level_names = ast.toplevel_names(top_level_node)
-            local location = top_level_node.loc
+            local node_location = top_level_node.loc
             for _, name in ipairs(top_level_names) do
                 local old_location = names[name]
                 if old_location then
-                    scope_error(location,
+                    scope_error(node_location,
                         "duplicate toplevel declaration for '%s', previous one at line %d",
                         name, old_location.line)
                 end
-                names[name] = location
+                names[name] = node_location
             end
         end
     end
@@ -258,6 +263,10 @@ function Checker:check_program(prog_ast)
     -- Add builtins to symbol table. (The order does not matter because they are distinct.)
     for name, _ in pairs(builtins) do
         self:add_builtin(name)
+    end
+
+    for name in pairs(builtin_modules) do
+        self:add_module(name)
     end
 
     -- Add a special entry for $tofloat which can never be shadowed and is therefore always visible.
@@ -569,6 +578,7 @@ function FunChecker:check_stat(stat)
                         "attempting to assign to builtin function %s",
                         stat.vars[i].name)
                 end
+                -- TODO: Add checker.Name.Module to prevent module assignment
             end
         end
 
@@ -627,8 +637,12 @@ function FunChecker:check_var(var)
             var._type = self.p.module.functions[cname.id].typ
         elseif cname._tag == "checker.Name.Builtin" then
             var._type = builtins[cname.name].typ
-               else
-            error("impossible")
+        elseif cname._tag == "checker.Name.Module" then
+            -- We ignore just the module name for now.
+            -- The type for a module is a table.
+            var._type = builtin_modules[cname.name]
+        else
+            error("Control should not reach here. Cannot handle: cname._tag == " .. cname._tag)
         end
 
     elseif tag == "ast.Var.Dot" then
