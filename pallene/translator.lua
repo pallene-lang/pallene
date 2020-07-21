7166f1a08f8aca96f9c8d96d14c83d6871c0ce1a
Samuel Rowe <samuelrowe1999@gmail.com>
2020-07-21 18:32:16 +0530

    Refactor the translator.
    The `translator.translate()` function was refactored into smaller functions to avoid duplication of
    code. Further, a few local variables were moved to the instance to avoid passing them around as
    parameters.

--- pallene/translator.lua
+++ pallene/translator.lua
@@ -31,6 +31,8 @@ local Translator = util.Class()
 function Translator:init()
     self.last_index = 1
     self.partials = {}
+    self.exports = {}
+    self.input = false
     return self
 end
 
@@ -46,15 +48,15 @@ function Translator:add_whitespace(input, start_index, stop_index)
     assert(start_index <= stop_index+1)
     self:add_previous(input, start_index - 1)
 
-    local region = input:sub(start_index, stop_index)
+    local region = self.input:sub(start_index, stop_index)
     local partial = region:gsub("%S", " ")
     table.insert(self.partials, partial)
 
     self.last_index = stop_index + 1
 end
 
-function Translator:add_local(input, start_index, stop_index)
-    self:add_previous(input, start_index - 1)
+function Translator:add_local(start_index, stop_index)
+    self:add_previous(start_index - 1)
     -- The export keyword is six characters long, whereas the local keyword is five characters
     -- long. Therefore, we pad the keyword with a space. We could add the space to the left, too.
     -- But it seems more "natural" on the right.
@@ -63,10 +65,10 @@ function Translator:add_local(input, start_index, stop_index)
     self.last_index = stop_index
 end
 
-function Translator:add_exports(exports)
-    if #exports > 0 then
+function Translator:add_exports()
+    if #self.exports > 0 then
         table.insert(self.partials, "\nreturn {\n")
-        for _, export in ipairs(exports) do
+        for _, export in ipairs(self.exports) do
             local pair = string.format("    %s = %s,\n", export, export)
             table.insert(self.partials, pair)
         end
@@ -74,72 +76,79 @@ function Translator:add_exports(exports)
     end
 end
 
+function Translator:translate_tl_variables(node)
+    -- Add the variables to the export sequence if they are declared with the `export`
+    -- modifier.
+    if not node.is_local then
+        self:add_local(node.loc.pos, node.mod_end_loc.pos - 1)
+        for _, decl in ipairs(node.decls) do
+            table.insert(self.exports, decl.name)
+        end
+    end
+
+    for _, decl in ipairs(node.decls) do
+        if decl.type then
+            -- Remove the colon but retain any adjacent comment to the right.
+            self:add_whitespace(decl.col_loc.pos, decl.col_loc.pos)
+            -- Remove the type annotation but exclude the next token.
+            self:add_whitespace(decl.type.loc.pos, decl.end_loc.pos - 1)
+        end
+    end
+end
+
+function Translator:translate_tl_function(node)
+    if not node.is_local then
+        self:add_local(node.loc.pos, node.mod_end_loc.pos - 1)
+        table.insert(self.exports, node.decl.name)
+    end
+
+    -- Remove type annotations from function parameters.
+    for _, arg_decl in ipairs(node.value.arg_decls) do
+        -- Type annotations are mandatory for function parameters.
+        -- Remove the colon but retain any adjacent comment to the right.
+        self:add_whitespace(arg_decl.col_loc.pos, arg_decl.col_loc.pos)
+        -- Remove the type annotation but exclude the next token.
+        self:add_whitespace(arg_decl.type.loc.pos, arg_decl.end_loc.pos - 1)
+    end
+
+    -- Remove type annotations from the return type, which is optional. However, `rt_col_loc`
+    -- and `rt_end_loc` are always set. Therefore, it is safe to replace without any checks.
+    self:add_whitespace(node.rt_col_loc.pos, node.rt_end_loc.pos - 1)
+
+    -- Remove type annotations from local declarations.
+    for _, statement in ipairs(node.value.body.stats) do
+        if statement._tag == "ast.Stat.Decl" then
+            for _, decl in ipairs(statement.decls) do
+                if decl.type then
+                    -- Remove the colon but retain any adjacent comment to the right.
+                    self:add_whitespace(decl.col_loc.pos, decl.col_loc.pos)
+                    -- Remove the type annotation but exclude the next token.
+                    self:add_whitespace(decl.type.loc.pos, decl.end_loc.pos - 1)
+                end
+            end
+        end
+    end
+end
+
 function translator.translate(input, prog_ast)
     local instance = Translator:new()
-    local exports = {}
+    instance.input = input
 
     for _, node in ipairs(prog_ast) do
         if node._tag == "ast.Toplevel.Var" then
-            -- Add the variables to the export sequence if they are declared with the `export`
-            -- modifier.
-            if not node.is_local then
-                instance:add_local(input, node.loc.pos, node.mod_end_loc.pos - 1)
-                for _, decl in ipairs(node.decls) do
-                    table.insert(exports, decl.name)
-                end
-            end
-
-            for _, decl in ipairs(node.decls) do
-                if decl.type then
-                    -- Remove the colon but retain any adjacent comment to the right.
-                    instance:add_whitespace(input, decl.col_loc.pos, decl.col_loc.pos)
-                    -- Remove the type annotation but exclude the next token.
-                    instance:add_whitespace(input, decl.type.loc.pos, decl.end_loc.pos - 1)
-                end
-            end
+            instance:translate_tl_variables(node)
         elseif node._tag == "ast.Toplevel.Func" then
-            if not node.is_local then
-                instance:add_local(input, node.loc.pos, node.mod_end_loc.pos - 1)
-                table.insert(exports, node.decl.name)
-            end
-
-            -- Remove type annotations from function parameters.
-            for _, arg_decl in ipairs(node.value.arg_decls) do
-                -- Type annotations are mandatory for function parameters.
-                -- Remove the colon but retain any adjacent comment to the right.
-                instance:add_whitespace(input, arg_decl.col_loc.pos, arg_decl.col_loc.pos)
-                -- Remove the type annotation but exclude the next token.
-                instance:add_whitespace(input, arg_decl.type.loc.pos, arg_decl.end_loc.pos - 1)
-            end
-
-            -- Remove type annotations from the return type, which is optional. However, `rt_col_loc`
-            -- and `rt_end_loc` are always set. Therefore, it is safe to replace without any checks.
-            instance:add_whitespace(input, node.rt_col_loc.pos, node.rt_end_loc.pos - 1)
-
-            -- Remove type annotations from local declarations.
-            for _, statement in ipairs(node.value.body.stats) do
-                if statement._tag == "ast.Stat.Decl" then
-                    for _, decl in ipairs(statement.decls) do
-                        if decl.type then
-                            -- Remove the colon but retain any adjacent comment to the right.
-                            instance:add_whitespace(input, decl.col_loc.pos, decl.col_loc.pos)
-                            -- Remove the type annotation but exclude the next token.
-                            instance:add_whitespace(input, decl.type.loc.pos, decl.end_loc.pos - 1)
-                        end
-                    end
-                end
-            end
+            instance:translate_tl_function(node)
         elseif node._tag == "ast.Toplevel.Typealias" then
             -- Remove the type alias but exclude the next token.
-            instance:add_whitespace(input, node.loc.pos, node.end_loc.pos - 1)
+            instance:add_whitespace(node.loc.pos, node.end_loc.pos - 1)
         elseif node._tag == "ast.Toplevel.Record" then
             -- Remove the record but exclude the next token.
-            instance:add_whitespace(input, node.loc.pos, node.end_loc.pos - 1)
+            instance:add_whitespace(node.loc.pos, node.end_loc.pos - 1)
         end
     end
     -- Whatever characters that were not included in the partials should be added.
-    instance:add_previous(input, #input)
-
+    instance:add_previous(#input)
     instance:add_exports(exports)
 
     return table.concat(instance.partials, "")
