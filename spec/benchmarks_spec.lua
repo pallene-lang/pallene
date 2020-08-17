61d8eadc4d9092ec61d5273e205689b277f2225b
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2020-08-17 12:27:03 -0300

    Simplify the benchmarks_spec a bit
    The function that we were calling assert_benchmark isn't really an assert because it isn't intended
    to be called inside an it() block. I renamed it to test_benchmark. At the same time, I also took the
    opportunity to move the describe() block inside of test_benchmark. Normally we don't do this kind of
    thing because the stack traces for all the test point to the same line number but in this case it is
    OK because all the test cases have clear names in the describe() and it().

--- spec/benchmarks_spec.lua
+++ spec/benchmarks_spec.lua
@@ -14,41 +14,36 @@ else
     print("Warning: not testing the LuaJIT benchmarks, because LuaJIT is not installed.")
 end
 
-local function assert_benchmark(bench, params, expected_output)
-    for _, impl in ipairs(impls) do
-        if benchlib.find_benchmark(bench, impl) then
-            it(impl, function()
+local function test_benchmark(bench, params, expected_output)
+    describe(bench .. " /", function()
+        for _, impl in ipairs(impls) do
+            if benchlib.find_benchmark(bench, impl) then
+                it(impl, function()
                 local out = benchlib.run_with_impl_name("none", bench, impl, params)
-                assert.are.same(expected_output, out)
-            end)
+                    assert.are.same(expected_output, out)
+                end)
+            end
         end
-    end
+    end)
 end
 
 
-describe("Binarytrees /", function()
-    assert_benchmark("binarytrees", {5, 1}, [[
+test_benchmark("binarytrees", {5, 1}, [[
 stretch tree of depth 7	 check: 255
 64	 trees of depth 4	 check: 1984
 16	 trees of depth 6	 check: 2032
 long lived tree of depth 6	 check: 127
 ]])
-end)
 
-describe("Binsearch /", function()
-    assert_benchmark("binsearch", {100, 10}, [[
+test_benchmark("binsearch", {100, 10}, [[
 10
 ]])
-end)
 
-describe("Centroid /", function()
-    assert_benchmark("centroid", {10, 1}, [[
+test_benchmark("centroid", {10, 1}, [[
 17.27825	17.27825
 ]])
-end)
 
-describe("Conway /", function()
-    assert_benchmark("conway", {1, 1}, [[
+test_benchmark("conway", {1, 1}, [[
 |            **   **       *          **   **       *          **   **       *   |
 | *    *    **   **   *     **  *    **   **   *     **  *    **   **   *     ** |
 |  **   **             **  **    **             **  **    **             **  **  |
@@ -90,17 +85,13 @@ describe("Conway /", function()
 | **   **             **        **             **        **             **       |
 |           *    *                   *    *                   *    *             |
 ]])
-end)
 
-describe("Fannkuch /", function()
-    assert_benchmark("fannkuchredux", {5, 1}, [[
+test_benchmark("fannkuchredux", {5, 1}, [[
 11
 Pfannkuchen(5) = 7
 ]])
-end)
 
-describe("Fasta /", function()
-    assert_benchmark("fasta", {20, 1}, [[
+test_benchmark("fasta", {20, 1}, [[
 >ONE Homo sapiens alu
 GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTT
 >TWO IUB ambiguity codes
@@ -109,38 +100,28 @@ cttBtatcatatgctaKggNcataaaSatgtaaaDcDRtBggDtctttataattcBgtcg
 tacgtgtagcctagtgtttgtgttgcgttatagtctatttgtggacacagtatggtcaaa
 tgacgtcttttgatctgacggcgttaacaaagatactctg
 ]])
-end)
 
-describe("mandelbrot /", function()
-    assert_benchmark("mandelbrot", {8, 1}, "P4\n8 8\n\x02\x00\x0f\x2f\xff\x2f\x0f\x00")
-end)
+test_benchmark("mandelbrot", {8, 1}, "P4\n8 8\n\x02\x00\x0f\x2f\xff\x2f\x0f\x00")
 
-describe("Matmul /", function()
-    assert_benchmark("matmul", {17, 1}, [[
+test_benchmark("matmul", {17, 1}, [[
 #C	17	17
 C[1][1]	20803.898903
 ]])
-end)
 
-describe("Nbody /", function()
-    assert_benchmark("nbody", {50, 1}, [[
+test_benchmark("nbody", {50, 1}, [[
 -0.169075164
 -0.169063618
 ]])
-end)
 
-describe("Objmandelbrot /", function()
-    assert_benchmark("objmandelbrot", {3, 1}, [[
+test_benchmark("objmandelbrot", {3, 1}, [[
 P2
 3 3 255
 0 0 0
 0 4 4
 0 1 1
 ]])
-end)
 
-describe("Queen /", function()
-    assert_benchmark("queen", {4, 1}, [[
+test_benchmark("queen", {4, 1}, [[
 - X - -
 - - - X
 X - - -
@@ -152,16 +133,11 @@ X - - -
 - X - -
 
 ]])
-end)
 
-describe("Sieve /", function()
-    assert_benchmark("sieve", {50, 1}, [[
+test_benchmark("sieve", {50, 1}, [[
 15
 ]])
-end)
 
-describe("Spectral Norm /", function()
-    assert_benchmark("spectralnorm", {20, 1}, [[
+test_benchmark("spectralnorm", {20, 1}, [[
 1.273839841
 ]])
-end)
