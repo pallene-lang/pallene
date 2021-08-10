36dd7591b28dbb71752411f2753f50bbe916008e
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2021-08-10 13:12:42 -0300

    Split the "contains" test into "start" and "contains"

--- spec/lexer_spec.lua
+++ spec/lexer_spec.lua
@@ -39,14 +39,17 @@ end
 describe("Pallene lexer", function()
 
     it("can lex some keywords", function()
-        assert_lex("and", {"and"}, {})
+        assert_lex("and",      {"and"},      {})
+        assert_lex("else",     {"else"},     {})
+        assert_lex("if",       {"if"},       {})
         assert_lex("function", {"function"}, {})
     end)
 
-    it("can lex keywords that contain other keywords", function()
-        assert_lex("if",     {"if"},     {})
-        assert_lex("else",   {"else"},   {})
+    it("can lex keywords that start with other keywords", function()
         assert_lex("elseif", {"elseif"}, {})
+    end)
+
+    it("can lex keywodds that contain other keywords", function()
         assert_lex("record", {"record"}, {}) -- (contains "or")
     end)
 
