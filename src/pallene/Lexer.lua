64dc0f72ff525d5183a37b61ff8787b356d3210f
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2023-05-23 18:54:41 -0300

    Allow long strings with ]]=]
    Fixes #554.

--- src/pallene/Lexer.lua
+++ src/pallene/Lexer.lua
@@ -28,8 +28,7 @@ local find_newline = (1 - newline)^0 * newline
 local comment_line = RE"[^\n\r]*" * newline^-1
 
 local longstring_open    = P("[") * P("=")^0 * P("[")
-local longstring_close   = P("]") * P("=")^0 * P("]")
-local longstring_content = (P(1) - longstring_close)^1
+local longstring_content = (1 - P("]"))^1 + P("]")
 
 local string_delimiter  = RE"[\"\']"
 local string_content    = RE"[^\"\'\n\r\\]+"
@@ -184,21 +183,15 @@ function Lexer:read_short_string(delimiter)
     return table.concat(parts)
 end
 
-function Lexer:read_long_string(delimiter_length, what)
+function Lexer:read_long_string(delimiter_size, what)
     local firstline = self.line
+    local close = "]" .. string.rep("=", delimiter_size) .. "]"
+
     self:try(newline)
     local parts = {}
-    while true do
-        if self:try(longstring_close) then
-            if #self.matched == delimiter_length then
-                break
-            else
-                table.insert(parts, self.matched)
-            end
-
-        elseif self:try(longstring_content) then
+    while not self:try(close) do
+        if self:try(longstring_content) then
             table.insert(parts, self.matched)
-
         else
             return false, string.format("unfinished %s (starting at line %d)", what, firstline)
         end
@@ -212,7 +205,7 @@ function Lexer:_next()
 
     elseif self:try("--") then
         if self:try(longstring_open) then
-            local s, err = self:read_long_string(#self.matched, "long comment")
+            local s, err = self:read_long_string(#self.matched-2, "long comment")
             if not s then return false, err end
             return "COMMENT", s
         else
@@ -226,7 +219,7 @@ function Lexer:_next()
         return "STRING", s
 
     elseif self:try(longstring_open) then
-        local s, err = self:read_long_string(#self.matched, "long string")
+        local s, err = self:read_long_string(#self.matched-2, "long string")
         if not s then return false, err end
         return "STRING", s
 
