b68c83893143d299978eed987d9f4e2eb80097ab
Ninad Sachania <ninad.sachania@gmail.com>
2026-08-10 15:27:42 +0000

    Use pallene-lua to run the tests

--- benchmarks/benchlib.lua
+++ benchmarks/benchlib.lua
@@ -2,7 +2,7 @@ local util = require "pallene.util"
 
 local benchlib = {}
 
-benchlib.DEFAULT_LUA = "lua"
+benchlib.DEFAULT_LUA = "pallene-lua"
 
 -- @param lua_path:       Lua interpreter to use
 -- @param benchmark_path: Path to the benchmark file
